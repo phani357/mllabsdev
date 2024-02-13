a45fc8d6808727f92c563681994eaccf78860572
phani <gandhamsaiphanindra@gmail.com>
2024-02-13 18:31:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'; // Import your CSS file with styles
+import './App.css';
 
-const textArray = [
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  // Add more sentences as needed
-];
+function App() {
+return (
+<div className="typewriter">
+<h1 className="heading"> WELCOME,HUMAN.. EMBRACE
+THE POSSIBILITIES THAT LIE AHEAD, FOR HERE, 
+THE CONVERGENCE OF HUMAN INGENUITY AND ARTIFICIAL INTELLIGENCE {'/n'} SPARKS A NEW ERA OF DISCOVERY.
+PREPARE TO UNLOCK THE DOORS OF {'/n'} PERCEPTION AND EMBARK UPON A JOURNEY THAT
+TRANSCENDS THE LIMITS OF
+THE TANGIBLE WORLD.</h1>
+</div>
+);
+}
 
-const SlidingText = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % textArray.length);
-    }, 30000); // Change the duration as needed (in milliseconds)
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <div className="sliding-text-container">
-      <div className="sliding-text">
-        {textArray.map((text, index) => (
-          <div
-            key={index}
-            className={`text-item ${index === currentIndex ? 'active' : ''}`}
-          >
-            {text}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default SlidingText;
+export default App;

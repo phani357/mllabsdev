3c239d027a3ef3ec781abe5187142efb5a05e77e
phani <gandhamsaiphanindra@gmail.com>
2024-02-23 17:48:09 +0530

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import  './App.css';
+import './girl.png';
 
-function App() {
-  const [inputText, setInputText] = useState('');
-  
-  const handleChange = (e) => {
-    setInputText(e.target.value);
+
+function UserInputValidation() {
+  // State to hold user input
+  const [inputValue, setInputValue] = useState('');
+
+  // Function to handle input change
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
   };
-  
+
+  // Function to determine the output based on input
+  const getOutput = () => {
+    if (inputValue.toLowerCase() === 'demo') {
+      return 'Let me show my skills, here are the list of actions to be performed';
+    } else {
+      return '';
+    }
+  };
+
   return (
     <div className='typewriter'>
-      <h1 className="heading"> WELCOME,HUMAN.. EMBRACE THE POSSIBILITIES THAT LIE AHEAD, FOR HERE, THE CONVERGENCE OF HUMAN INGENUITY AND ARTIFICIAL INTELLIGENCE </h1>
-      <p className='heading'>HT! TYPE'DEMO' FOR SHOWCASING MY CAPABILITIES!</p>
-      <input className='input' type="text" value={inputText} onChange={handleChange} />
-      <p className='heading'> Your input matters: {inputText}</p>
+      <label className='heading'>
+      WELCOME,HUMAN.. EMBRACE THE POSSIBILITIES THAT LIE AHEAD, FOR HERE, THE CONVERGENCE OF HUMAN INGENUITY AND ARTIFICIAL 
+      <input className='input' type="text" placeholder="Enter your text here" value={inputValue} onChange={handleInputChange} />
+      </label>
+      
+      {/* Display result based on input */}
+      <p className='input'>    {getOutput()}</p>
+     <img className='topRightImage' src='/src/girl.png' alt='1'/>
     </div>
   );
 }
 
-export default App;
+export default UserInputValidation;
